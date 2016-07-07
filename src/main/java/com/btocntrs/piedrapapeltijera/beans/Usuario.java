288cdfed578ca2b2ab8b70b3031ffe852038f9d0
Beto Contreras <btocntrs@hotmail.com>
2016-07-06 20:22:39 -0500

    Se creo la clase Usuario con los atributos id, nombre, email y telefono

---
+++ src/main/java/com/btocntrs/piedrapapeltijera/beans/Usuario.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.btocntrs.piedrapapeltijera.beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rider
+ */
+public class Usuario implements Serializable{
+    
+    private long id;
+    private String nombre;
+    private String email;
+    private String telefono;
+
+    public Usuario() {
+    }
+
+    public Usuario(long id, String nombre, String email, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
